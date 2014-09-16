0daa2f971d2f75eb01973c8e6751f6e786e1988b
Jacquelinehy520 <jacquelinehy520@gmail.com>
2014-09-15 23:43:22 -0400

    scripts

---
+++ Scripts/Test_CSharp.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test_CSharp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
+
+
+
+/* Study notes:
+ * 1. we can use debug.log("") to detect bugs
+ * 2. use of switch
+ * 		switch(sth){
+ * 			case 1:
+ * 				break;
+ * 			case 2:
+ * 				break;
+ * 			case 3:
+ * 				break;
+ * 			case 4:
+ * 				break;
+ * 			case n:
+ * 				break;
+ * 			default:
+ * 				break;
+ * 		}
+ * 
+ * 3. parsing error: check the brackets first
+ * 4. Custom functions
+ * 5. Input.GetKeyUp("space")  	Input.GetKeyDown("space")
+ * 6. Loops
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
